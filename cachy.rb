ad2ffd3c686086484b78447e007f0092fa829edf
son <leson.phung@gmail.com>
2011-07-16 10:42:26 +0800

    Init cachy.rb.

---
+++ cachy.rb
@@ -0,0 +1,157 @@
+module Cachy
+  extend ActiveSupport::Concern
+
+  def self.cache
+    @cache ||= Rails.cache
+  end
+
+  def self.cache=(cache)
+    @cache = cache
+  end
+
+  def self.digest(key, options = {})
+    key = key.map { |v| "#{v}" }.join(':') if key.is_a?(::Array)
+    key = key.sort_by { |k, v| "#{k}" }.join(':') if key.is_a?(::Hash)
+    key = "#{key}" unless key.is_a?(::String)
+
+    key = "version:#{Ryokan::Application::CACHE_CONFIG[:version]}:#{key}" unless options[:no_version]
+    key = "locale:#{I18n.locale}:#{key}" unless options[:no_locale]
+
+    key = Digest::SHA1.hexdigest(key) unless options[:no_sha]
+
+    key
+  end
+
+  def self.autoload(object)
+    # In console, somehow we have missing constants error.
+    # It doesn't happen in production though.
+    if object.frozen? && object.is_a?(::String) && object =~ /ActiveSupport::Cache::Entry/
+      obj = Marshal.load(object)
+      return obj.value
+    else
+      object
+    end
+  rescue ArgumentError => error
+    lazy_load ||= Hash.new { |hash, hash_key| hash[hash_key] = true; false }
+
+    if error.to_s[/undefined class|referred/] && !lazy_load[error.to_s.split.last.constantize]
+      retry
+    else
+      raise error
+    end
+  end
+
+  def self.cache_option_keys
+    @cache_option_keys ||= [:expires_in]
+  end
+
+  def self.digest_option_keys
+    @digest_option_keys ||= [:no_version, :no_locale, :no_sha]
+  end
+
+  module ClassMethods
+    def cachy_options
+      @cachy_options ||= { :expires_in => 1.day, :no_locale => true }
+    end
+
+    def caches_method(name, options = {}, &block)
+      class_key = "#{self.name}:#{name}"
+
+      name_no_cache = "#{name}_no_cache"
+
+      options.reverse_merge!(cachy_options)
+
+      condition = options[:if]
+      key = options[:key]
+
+      class_eval do
+        alias_method name_no_cache, name
+
+        define_method name do |*args|
+          cache_key = key ? self.send(key) : (block && block.call(self, *args))
+          cache_key = ::Cachy.digest(cache_key, options.slice(*::Cachy.digest_option_keys))
+
+          variable = "@cachy_#{name}_#{cache_key}"
+          unless instance_variable_defined?(variable)
+            object = if condition && condition.call(self, *args) == false
+              send(name_no_cache, *args)
+            else
+              Rails.logger.info "#{class_key}:#{cache_key}"
+              Rails.logger.info options.slice(*::Cachy.cache_option_keys).inspect
+              obj = ::Cachy.cache.fetch("#{class_key}:#{cache_key}", options.slice(*::Cachy.cache_option_keys)) do
+                send(name_no_cache, *args)
+              end
+              ::Cachy.autoload(obj)
+            end
+
+            instance_variable_set(variable, object)
+          end
+
+          instance_variable_get(variable)
+        end
+
+        define_method "clear_cache_#{name}" do |*args|
+          cache_key = key ? self.send(key) : block && block.call(self, *args)
+          cache_key = ::Cachy.digest(cache_key, options.slice(*::Cachy.digest_option_keys))
+
+          variable = "@cachy_#{name}_#{cache_key}"
+          remove_instance_variable(variable) if instance_variable_defined?(variable)
+
+          ::Cachy.cache.delete("#{class_key}:#{cache_key}")
+        end
+      end
+
+    end
+
+    def caches_methods(*names, &block)
+      options = names.extract_options!
+      names.each do |name|
+        caches_method(name, options, &block)
+      end
+    end
+
+    def caches_class_method(name, options = {}, &block)
+      options.reverse_merge!(cachy_options)
+      condition = options[:if]
+
+      class_key = "#{self.name}:class:#{name}"
+      metaclass.instance_eval do
+        define_method name do |*args|
+          cache_key = block ? block.call(*args) : args
+          cache_key = ::Cachy.digest(cache_key, options.slice(*::Cachy.digest_option_keys))
+
+          object = if condition && condition.call(*args) == false
+            super(*args)
+          else
+            Rails.logger.info "#{class_key}:#{cache_key}"
+            Rails.logger.info options.slice(*::Cachy.cache_option_keys).inspect
+            obj = ::Cachy.cache.fetch("#{class_key}:#{cache_key}", options.slice(*::Cachy.cache_option_keys)) do
+              super(*args)
+            end
+
+            ::Cachy.autoload(obj)
+          end
+
+
+          object
+        end
+
+        define_method "clear_cache_#{name}" do |*args|
+          cache_key = block ? block.call(*args) : args
+          cache_key = ::Cachy.digest(cache_key, options.slice(*::Cachy.digest_option_keys))
+
+          ::Cachy.cache.delete("#{class_key}:#{cache_key}")
+        end
+      end
+
+    end
+
+    def caches_class_methods(*names, &block)
+      options = names.extract_options!
+      names.each do |name|
+        caches_class_method(name, options, &block)
+      end
+    end
+  end
+
+end
