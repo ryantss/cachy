a459d29a65226d1049df57ce900e6c2af611ab20
son <leson.phung@gmail.com>
2011-10-17 18:04:54 +0800

    Un-pro mistake in naming the gem.

--- tasks/distribution.rb
+++ tasks/distribution.rb
@@ -2,7 +2,7 @@
 # YARD::Rake::YardocTask.new
 
 Jeweler::Tasks.new do |gem|
-  gem.name        = "Cachy"
+  gem.name        = "cachy"
   gem.summary     = "Ruby method cache library"
   gem.description = "A library includes a helper to cache any methods in Ruby (mostly Rails)"
   gem.author      = "Son"
